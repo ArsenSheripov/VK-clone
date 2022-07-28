20b8e30303d95a9e2c0d798b1f7ce8de85a85b8d
Arsen <Arcenk3x@mail.ru>
2022-07-28 09:44:04 +0500

    friends

---
+++ src/pages/friends/FriendItem.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+import { IUser } from '../../types/IUser';
+import cl from './friends.module.scss';
+
+interface FriendItemProps {
+	friend: IUser
+}
+
+
+const FriendItem: FC<FriendItemProps> = ({ friend }) => {
+	return (
+		<div className={cl.friends__item}>
+			<div className={cl.friends__avatar}>
+				<img src={friend.avatar} alt="avatar" />
+				{friend.isInNetwork ? <span className={cl.friends__network}></span> : false}
+			</div>
+			<span className={cl.friends__name}>{friend.name}</span>
+		</div>
+	)
+}
+
+export default FriendItem;
--- src/pages/friends/Friends.tsx
+++ src/pages/friends/Friends.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { IUser } from '../../types/IUser';
+import defaultAvatar from '../../assets/defaultAvatar.jpg';
+import cl from './friends.module.scss';
+import FriendItem from './FriendItem';
 
 const Friends = () => {
+	const friends: IUser[] = [
+		{ id: '2', avatar: defaultAvatar, name: 'Halid', isInNetwork: true },
+		{ id: '3', avatar: defaultAvatar, name: 'Mar`am', isInNetwork: true },
+		{ id: '4', avatar: defaultAvatar, name: 'Zaur', isInNetwork: true },
+		{ id: '5', avatar: defaultAvatar, name: 'Madina', isInNetwork: true },
+	]
 	return (
-		<div>
-			friends
+		<div className={cl.friends}>
+			{friends.map(friend =>
+				<FriendItem key={friend.id} friend={friend} />
+			)}
 		</div>
 	)
 }
 
-export default Friends
+export default Friends;
