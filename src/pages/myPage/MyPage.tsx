e0f3fce2d221a8d047453a050a3222ae650ca541
Arsen <Arcenk3x@mail.ru>
2022-07-30 21:55:13 +0500

    my page added

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import cl from './myPage.module.scss';
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 const MyPage = () => {
+	const user = useAppSelector(state => state.user)
+
 	return (
-		<div>
-			MyPage
+		<div className={cl.myPage}>
+			<img src={user.avatar} className={cl.myPage__avatar} alt="avatar" />
+			<span className={cl.myPage__email}>{user.email}</span>
 		</div>
 	)
 }
